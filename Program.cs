8d0e5a8f8533c97428928653f2e64a5bb236172b
y.bogdanov <y.bogdanov@gmail.com>
2020-02-18 14:38:35 +0300

    .

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@ namespace WebApplication1
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+
                 });
     }
 }
